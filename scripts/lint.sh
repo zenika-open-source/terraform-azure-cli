3849ff05d90b9662c0fbb46d0d68ba64bddaf29a
Jan200101 <sentrycraft123@gmail.com>
2019-10-08 09:54:43 +0200

    Allow docker to output to stdout and stderr

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 if [ -f /.dockerenv ]; then
   hadolint Dockerfile
 else
-  docker run -v $(pwd):/host:ro hadolint/hadolint:v1.17.2-debian hadolint /host/Dockerfile
+  docker run -a stdout -a stderr -v $(pwd):/host:ro hadolint/hadolint:v1.17.2-debian hadolint /host/Dockerfile
 fi
